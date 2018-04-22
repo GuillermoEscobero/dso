b906d7c4add2bcb0ebe77a1a15c3714605d01979
RaulOlmedoCheca <raul.olmedo.checa@gmail.com>
2018-04-22 14:15:39 +0200

    Added some more tests, commit de mierda para que no explote

--- dssoo_fs/test.c
+++ dssoo_fs/test.c
@@ -37,7 +37,10 @@ int main() {
      * @description Create a file system
      */
     TEST_PRINT("1", "F1.1");
-    if (mkFS(DEV_SIZE) == 0) {
+    //TODO: help @guillermoescobero
+    printf("mkFS(DEV_SIZE + 1024) %d", mkFS(DEV_SIZE + BLOCK_SIZE * 40));
+    printf("mkFS(DEV_SIZE) %d", mkFS(DEV_SIZE));
+    if (mkFS(DEV_SIZE + 1024) == -1 && mkFS(DEV_SIZE) == 0) {
         TEST_PASSED("mkFS");
     } else {
         TEST_FAILED("mkFS");
@@ -61,7 +64,7 @@ int main() {
      * @description Create a file within the file system
      */
     TEST_PRINT("4", "F1.4");
-    if (createFile("test.txt") == 0) {
+    if (createFile("test.txt") == 0 && createFile("test.txt") == -1) {
         TEST_PASSED("createFile");
     } else {
         TEST_FAILED("createFile");
@@ -73,16 +76,17 @@ int main() {
      * @description Open an existing file
      */
     TEST_PRINT("6", "F1.6");
-    int fd = openFile("test.txt");
-    if (fd != -1 && fd != -2) {
+    int testFileDescriptor = openFile("test.txt");
+    if (testFileDescriptor == 0 && openFile("random.txt") == -1) {
         TEST_PASSED("openFile");
     } else {
         TEST_FAILED("openFile");
     }
 
-    int fd2 = open("quijote.txt", O_RDONLY);
+    // Fill the buffer with text using C standard functions to avoid introducing errors
+    int quijoteFileDescriptor = open("quijote.txt", O_RDONLY);
     char write_buffer[3000];
-    read(fd2, write_buffer, sizeof(write_buffer));
+    read(quijoteFileDescriptor, write_buffer, sizeof(write_buffer));
 
     /**
      * @test 9
@@ -90,9 +94,9 @@ int main() {
      * @description Write to an opened file, A file could be modified by means of write operations.
      */
     TEST_PRINT("9", "F1.9, F7");
-    if (writeFile(fd, write_buffer, sizeof(write_buffer) - 64) == 0) {
-        lseekFile(fd, 0, FS_SEEK_BEGIN);
-        if (writeFile(fd, write_buffer, 64) == 64) {
+    if (writeFile(testFileDescriptor, write_buffer, sizeof(write_buffer) - sizeof(char) * 8) == 0) {
+        lseekFile(testFileDescriptor, 0, FS_SEEK_BEGIN);
+        if (writeFile(testFileDescriptor, write_buffer, sizeof(char) * 8) == sizeof(char) * 8) {
             TEST_PASSED("writeFile");
         } else {
             TEST_FAILED("writeFile");
@@ -107,27 +111,28 @@ int main() {
      * @description Read from an opened file
      */
     TEST_PRINT("8", "F1.8");
-    char read_buffer[3000];
-    //FIXME:
-    int debug = readFile(fd, read_buffer, sizeof(read_buffer) - 64);
-    printf("Debug %d", debug);
-    lseekFile(fd, 0, FS_SEEK_BEGIN);
-    debug = readFile(fd, read_buffer, sizeof(read_buffer));
-    printf("Debug %d", debug);
-    debug = readFile(fd, read_buffer, sizeof(read_buffer));
-    printf("Debug %d", debug);
+    char read_buffer[64];
 
-    if (debug == 0) {
-        printf("Efectivamente");
-        lseekFile(fd, 0, FS_SEEK_BEGIN);
-        if (readFile(fd, read_buffer, sizeof(read_buffer)) == 0) {
-            TEST_PASSED("readFile");
+    //FIXME: no actualiza el seekpointer
+    int a = readFile(testFileDescriptor, read_buffer, sizeof(char) * 3);
+    printf("%s\n", read_buffer);
+    int b = readFile(testFileDescriptor, read_buffer, sizeof(char) * 3);
+    printf("%s\n", read_buffer);
+    int c = readFile(testFileDescriptor, read_buffer, sizeof(char) * 2);
+    printf("%s\n", read_buffer);
+    if (a == sizeof(char) * 5) {
+        if (b == sizeof(char) * 2) {
+            if (c == 0) {
+                TEST_PASSED("readFile");
+            } else {
+                TEST_FAILED("readFile 1");
+            }
         } else {
-            printf("faileo en este");
-            TEST_FAILED("readFile");
+            TEST_FAILED("readFile 2");
         }
     } else {
-        TEST_FAILED("readFile");
+        TEST_FAILED("readFile 3");
+        printf("a %d b %d c %d \n", a, b, c);
     }
 
     /**
@@ -136,11 +141,10 @@ int main() {
      * @description Modify the position of the seek pointer
      */
     TEST_PRINT("10", "F1.10");
-    char read_string[6];
+    char read_string[8];
     char *init_string = "#INIT#";
-    if (lseekFile(fd, 10, FS_SEEK_BEGIN) == 0) {
-        readFile(fd, read_string, 6);
-        printf("\nread string: %s ini%s\n", read_string, init_string);
+    if (lseekFile(testFileDescriptor, 10, FS_SEEK_BEGIN) == 0) {
+        readFile(testFileDescriptor, read_string, sizeof(char) * 6);
         if (strcmp(read_string, init_string) == 0) {
             TEST_PASSED("lseekFile");
         } else {
@@ -156,7 +160,7 @@ int main() {
      * @description Close an opened file
      */
     TEST_PRINT("7", "F1.7");
-    if (closeFile(fd2) == 0) {
+    if (closeFile(testFileDescriptor) == 0) {
         TEST_PASSED("closeFile");
     } else {
         TEST_FAILED("closeFile");
@@ -167,19 +171,59 @@ int main() {
      * @requirement F2
      * @description Every time a file is opened, its seek pointer will be reset to the beginning of the file.
      */
+    TEST_PRINT("12", "F2");
+    testFileDescriptor = openFile("test.txt");
+    //TODO: if readfile reads #init# esta ok
 
     /**
      * @test 11
      * @requirement F1.11
      * @description Check the integrity an existing file
      */
-    //TEST_PRINT("11", "F1.11");
-    //ret = checkFile("test.txt");
-    //if(ret != 0) {
-    //	TEST_FAILED("checkFile");
-    //	return -1;
-    //}
-    //TEST_PASSED("checkFile");
+    TEST_PRINT("11", "F1.11");
+    if (checkFile("test.txt") == -2) {
+        closeFile(testFileDescriptor);
+        if (checkFile("test.txt") == 0) {
+            //FIXME: no se como se usa bwrite hulio
+            bwrite("disk.dat", 2, write_buffer);
+            if (checkFile("test.txt") == -1) {
+                TEST_PASSED("checkFile");
+            } else {
+                TEST_FAILED("checkFile");
+            }
+        } else {
+            TEST_FAILED("checkFile");
+        }
+    } else {
+        TEST_FAILED("checkFile");
+    }
+
+    /**
+     * @test 5
+     * @requirement F1.5
+     * @description Remove an existing file from the file system
+     */
+    TEST_PRINT("5", "F1.5");
+    if (removeFile("test.txt") == 0 && removeFile("test.txt") == -1) {
+        TEST_PASSED("removeFile");
+    } else {
+        TEST_FAILED("removeFile");
+    }
+
+    /**
+     * @test 15
+     * @requirement F5
+     * @description File integrity must be checked, at least, on open operations.
+     */
+    TEST_PRINT("15", "F5");
+    createFile("test.txt");
+    //FIXME: no se como se usa bwrite hulio
+    bwrite("disk.dat", 2, write_buffer);
+    if (openFile("test.txt") == -2) {
+        TEST_PASSED("integrity check on openFile");
+    } else {
+        TEST_FAILED("integrity check on openFile");
+    }
 
     /**
      * @test 13
@@ -192,12 +236,6 @@ int main() {
      * @test 14
      * @requirement F4
      * @description The file system will not implement directories.
-     */
-
-    /**
-     * @test 15
-     * @requirement F5
-     * @description File integrity must be checked, at least, on open operations.
      */
 
     /**
@@ -230,50 +268,67 @@ int main() {
     //	TEST_FAILED("unmountFS");
     //}
 
-    /**
-     * @test 5
-     * @requirement F1.5
-     * @description Remove an existing file from the file system
-     */
-    //TEST_PRINT("5", "F1.5");
-    //if(removeFile("test.txt") == 0){
-    //	TEST_PASSED("removeFile");
-    //} else {
-    //	TEST_FAILED("removeFile");
-    //}
+
+    close(quijoteFileDescriptor);
 
     //NON-FUNCTIONAL REQUIREMENTS TESTS
+    fprintf(stdout, "%s", "BASIC TESTING OF NON-FUNCTIONAL REQUIREMENTS\n\n");
 
-    //TODO:
-    //printf("BASIC TESTING OF NON-FUNCTIONAL REQUIREMENTS\n");
+    /**
+     * @test 19
+     * @requirement NF1
+     * @description The maximum number of files in the file system will never be higher than 40.
+     */
+    TEST_PRINT("19", "NF1");
 
-    //printf("TEST NUMBER %d: NON FUNCTIONAL REQUIREMENT 1\n", i);
-    //char* string = "File ";
-    //for (int j = 1; j <= 40; ++j) {
-    //	strcat(string, j);
-    //	ret = createFile(string);
-    //}
-    //i++;
+    /**
+     * @test 20
+     * @requirement NF2
+     * @description The maximum number of files in the file system will never be higher than 40.
+     */
+    TEST_PRINT("20", "NF1");
 
-    /////////Maximum length of file name is 32 characters NF2  ERROR EXPECTED
-    //printf("TEST NUMBER %d: NON FUNCTIONAL REQUIREMENT 2\n", i);
-    //ret = createFile("abcdefghijklmnopqrstuvwxyzabcdefg"); //33 characters
-    //if(ret == 0) {
-    //	TEST_FAILED("create (created the file anyway)");
-    //	return -1;
-    //}
-    //ret = createFile("abcdefghijklmnopqrstuvwxyzabcde"); //31 characters
-    //if(ret != 0) {
-    //	TEST_FAILED("create");
-    //	fprintf(stdout, "%s%s%s%s%s", ANSI_COLOR_BLUE, "TEST create ", ANSI_COLOR_RED, "FAILED\n", ANSI_COLOR_RESET);
-    //	return -1;
-    //}
-    //TEST_PASSED("create");
-    //i++;
-    //It's already been unmounted so we mount it again
-    //ret = mountFS();
-    //Now check metadata
+    /**
+     * @test 21
+     * @requirement NF3
+     * @description The maximum number of files in the file system will never be higher than 40.
+     */
+    TEST_PRINT("21", "NF1");
 
-    close(fd2);
+    /**
+     * @test 22
+     * @requirement NF4
+     * @description The maximum number of files in the file system will never be higher than 40.
+     */
+    TEST_PRINT("22", "NF1");
+
+    /**
+     * @test 23
+     * @requirement NF5
+     * @description The maximum number of files in the file system will never be higher than 40.
+     */
+    TEST_PRINT("23", "NF1");
+
+    /**
+     * @test 24
+     * @requirement NF6
+     * @description The maximum number of files in the file system will never be higher than 40.
+     */
+    TEST_PRINT("24", "NF1");
+
+    /**
+     * @test 25
+     * @requirement NF7
+     * @description The maximum number of files in the file system will never be higher than 40.
+     */
+    TEST_PRINT("25", "NF1");
+
+    /**
+     * @test 26
+     * @requirement NF8
+     * @description The maximum number of files in the file system will never be higher than 40.
+     */
+    TEST_PRINT("26", "NF1");
+
     return 0;
 }
